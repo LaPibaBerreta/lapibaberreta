6363d75570323cab31a99a89452c4d8de4d6d5f5
astrosuka <yegresvoselok@gmail.com>
2025-11-17 03:32:55 -0300

    Add video player to videos page

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,6 +1,7 @@
 import Loading from "../components/Loading";
 import { useVideos } from "../hooks/useVideos";
 import type { InitialDataQueryResult } from "@/lib/types";
+import VideoPlayer from "../components/VideoPlayer";
 
 type Section = NonNullable<
   NonNullable<InitialDataQueryResult>["sections"]
@@ -17,12 +18,15 @@ export default function Videos({ section }: { section: Section }) {
       {section.title && <h1 className="text-xl">{section.title.es}</h1>}
       <div className="flex gap-2">
         {data &&
-          data.map((video) => (
-            <div key={video._id} className="border p-4">
-              <h2>{video.title?.es}</h2>
-              <div className="text-xs">{video.category?.name?.es}</div>
-            </div>
-          ))}
+          data.map((video) =>
+            video.embed ? (
+              <div key={video._id} className="border p-4">
+                <h2>{video.title?.es}</h2>
+                <div className="text-xs">{video.category?.name?.es}</div>
+                <VideoPlayer embedData={video.embed} />
+              </div>
+            ) : null,
+          )}
       </div>
     </>
   );
