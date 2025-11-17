182faab3983414f7f394dff36cbfbad532fed825
astrosuka <yegresvoselok@gmail.com>
2025-11-17 17:45:18 -0300

    Add cards data to oracle

--- src/pages/Oracle.tsx
+++ src/pages/Oracle.tsx
@@ -2,6 +2,7 @@ import type { InitialDataQueryResult } from "@/lib/types";
 import { useOraculo } from "../hooks/useOraculo";
 import Loading from "../components/Loading";
 import { PortableText } from "@portabletext/react";
+import { urlFor } from "../lib/sanityImageUrl";
 
 type Section = NonNullable<
   NonNullable<InitialDataQueryResult>["sections"]
@@ -17,6 +18,23 @@ export default function Oracle({ section }: { section: Section }) {
     <>
       {section.title && <h1 className="text-xl">{section.title.es}</h1>}
       {data?.text?.es && <PortableText value={data.text.es} />}
+      <div className="my-6 grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-4">
+        {data?.cards?.length &&
+          data.cards.map((card) => (
+            <div key={card._key} className="border-2">
+              {card.title?.es && <h2>{card.title.es}</h2>}
+              {card.text?.es && <PortableText value={card.text.es} />}
+              {card.image && (
+                <img
+                  src={
+                    urlFor(card.image).format("webp").width(400).url() +
+                    "&fit=max"
+                  }
+                />
+              )}
+            </div>
+          ))}
+      </div>
     </>
   );
 }
