f17f2541f4a55cabf16115e0fe70d07cd35f239a
astrosuka <yegresvoselok@gmail.com>
2025-12-19 17:31:40 -0300

    Load section slug data before loading graph component to avoid undefined path

--- src/graph/GraphSection.tsx
+++ src/graph/GraphSection.tsx
@@ -5,6 +5,7 @@ import { useWorkshops } from "../hooks/useWorkshops";
 import { buildGraph } from "./buildGraph";
 import { Graph } from "./Graph";
 import type { GraphInputData } from "./types/Graph";
+import { useSectionSlug } from "../hooks/useSectionSlug";
 
 export default function GraphSection() {
   const { data: sections, isLoading, error } = useInitialData();
@@ -19,10 +20,30 @@ export default function GraphSection() {
     isLoading: wsLoading,
     error: wsError,
   } = useWorkshops();
+  const {
+    data: sectionSlug,
+    isLoading: isSectionSlugLoading,
+    error: sectionSlugError,
+  } = useSectionSlug();
 
-  if (isLoading || pubLoading || vidLoading || wsLoading) return <div>...</div>;
-  if (error || pubError || vidError || wsError)
-    return <div>{error?.message || pubError?.message || wsError?.message}</div>;
+  if (
+    isLoading ||
+    pubLoading ||
+    vidLoading ||
+    wsLoading ||
+    isSectionSlugLoading ||
+    !sectionSlug
+  )
+    return <div>...</div>;
+  if (error || pubError || vidError || wsError || sectionSlugError)
+    return (
+      <div>
+        {error?.message ||
+          pubError?.message ||
+          wsError?.message ||
+          sectionSlugError?.message}
+      </div>
+    );
 
   const graphInputData: GraphInputData = {
     sections: sections?.sections ?? [],
