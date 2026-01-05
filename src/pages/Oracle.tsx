46c6e2e1fb4936940b2c225b22fce798676f0b44
astrosuka <yegresvoselok@gmail.com>
2026-01-05 20:51:10 -0300

    Add random card selection functionality to Oracle

--- src/pages/Oracle.tsx
+++ src/pages/Oracle.tsx
@@ -1,4 +1,5 @@
-import type { InitialDataQueryResult } from "@/lib/types";
+import { useState } from "react";
+import type { InitialDataQueryResult, OraculoQueryResult } from "@/lib/types";
 import { useOraculo } from "../hooks/useOraculo";
 import Loading from "../components/Loading";
 import { PortableText } from "@portabletext/react";
@@ -8,33 +9,45 @@ type Section = NonNullable<
   NonNullable<InitialDataQueryResult>["sections"]
 >[number];
 
+type OracleCard = NonNullable<NonNullable<OraculoQueryResult>["cards"]>[number];
+
 export default function Oracle({ section }: { section: Section }) {
   const { data, isLoading, error } = useOraculo();
+  const [currentCard, setCurrentCard] = useState<OracleCard | null>(null);
 
   if (isLoading) return <Loading />;
   if (error) return <div>error.message</div>;
+
+  const handleGetCard = () => {
+    if (!data?.cards?.length) return;
+    setCurrentCard(
+      data?.cards?.[Math.floor(Math.random() * data?.cards?.length)],
+    );
+  };
 
   return (
     <>
       {section.title && <h1 className="text-xl">{section.title.es}</h1>}
       {data?.text?.es && <PortableText value={data.text.es} />}
-      <div className="my-6 grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-4">
-        {data?.cards?.length &&
-          data.cards.map((card) => (
-            <div key={card._key} className="border-2">
-              {card.title?.es && <h2>{card.title.es}</h2>}
-              {card.text?.es && <PortableText value={card.text.es} />}
-              {card.image && (
-                <img
-                  src={
-                    urlFor(card.image).format("webp").width(400).url() +
-                    "&fit=max"
-                  }
-                />
-              )}
-            </div>
-          ))}
-      </div>
+
+      <button className="my-2 border px-2" onClick={handleGetCard}>
+        sacar carta
+      </button>
+
+      {currentCard && (
+        <div key={currentCard._key} className="border-2">
+          {currentCard.title?.es && <h2>{currentCard.title.es}</h2>}
+          {currentCard.text?.es && <PortableText value={currentCard.text.es} />}
+          {currentCard.image && (
+            <img
+              src={
+                urlFor(currentCard.image).format("webp").width(400).url() +
+                "&fit=max"
+              }
+            />
+          )}
+        </div>
+      )}
     </>
   );
 }
