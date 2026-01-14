1e43f347386d9c7d2de89840cc9ddc090ffa4ed7
astrosuka <yegresvoselok@gmail.com>
2026-01-14 19:29:45 -0300

    Add motion and icon to ProjectSelectMenu

--- src/components/ProjectSelectMenu.tsx
+++ src/components/ProjectSelectMenu.tsx
@@ -1,6 +1,8 @@
 import { useProjects } from "../hooks/useProjects";
 import useFilterByProject from "../hooks/useFilterByProject";
 import useLanguage from "../hooks/useLanguage";
+import { X } from "lucide-react";
+import { motion } from "motion/react";
 
 export default function ProjectSelectMenu() {
   const {
@@ -22,7 +24,12 @@ export default function ProjectSelectMenu() {
             key={project._id}
             className="pointer-events-auto flex items-center gap-1 text-2xl"
           >
-            <button
+            <motion.button
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              whileHover={{ scale: 1.025 }}
+              whileTap={{ scale: 0.985 }}
               className={`min-w-12 cursor-pointer rounded-4xl border px-2 transition-colors hover:bg-black hover:text-white ${selectedProject === project._id ? "bg-black text-white" : "bg-white"}`}
               onClick={() => {
                 setSelectedProject(project._id);
@@ -30,19 +37,24 @@ export default function ProjectSelectMenu() {
             >
               {project.title?.es &&
                 (project.title[language] || project.title?.es)}
-            </button>
+            </motion.button>
           </div>
         ))}
       {selectedProject && (
         <div className="pointer-events-auto flex items-center gap-1 text-2xl">
-          <button
-            className={`min-w-12 cursor-pointer rounded-4xl border bg-white/40 transition-colors hover:bg-black hover:text-white`}
+          <motion.button
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            className={`_min-w-12 _hover:bg-black _hover:text-white cursor-pointer rounded-4xl border bg-white/40 transition-colors`}
             onClick={() => {
               setSelectedProject(null);
             }}
           >
-            X
-          </button>
+            <X strokeWidth={1.5} size={32} />
+          </motion.button>
         </div>
       )}
     </div>
