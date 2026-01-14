8e70b0442785db6425ed8be4a89e337a0fde3873
astrosuka <yegresvoselok@gmail.com>
2026-01-14 19:58:38 -0300

    Refactor Videos page to improve UI, remove links to single video page and insert button to view details outside of the main grid

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,14 +1,12 @@
+import { useState } from "react";
 import Loading from "../components/Loading";
 import { useVideos } from "../hooks/useVideos";
-import type { InitialDataQueryResult } from "@/lib/types";
+import type { InitialDataQueryResult, VideosQueryResult } from "@/lib/types";
 import VideoPlayer from "../components/VideoPlayer";
-import { PortableText } from "@portabletext/react";
-import { NavLink } from "react-router";
-import { useInitialData } from "../hooks/useInitialData";
-import { SECTION_IDS } from "../data/constants";
 import useFilterByProject from "../hooks/useFilterByProject";
 import useLanguage from "../hooks/useLanguage";
 import { useProjects } from "../hooks/useProjects";
+import VideoInfoPanel from "../components/VideoInfoPanel";
 
 type Section = NonNullable<
   NonNullable<InitialDataQueryResult>["sections"]
@@ -16,19 +14,23 @@ type Section = NonNullable<
 
 export default function Videos({ section }: { section: Section }) {
   const { data, isLoading, error } = useVideos();
-  const { data: initialData } = useInitialData();
   const { data: projectsData, isLoading: projectsDataLoading } = useProjects();
   const { selectedProject } = useFilterByProject();
   const { language } = useLanguage();
-
-  const videosSection = initialData?.sections?.find(
-    (section) => section.reference?._id === SECTION_IDS.VIDEOS,
-  );
+  const [isInfoPanelOpen, setIsInfoPanelOpen] = useState(false);
+  const [selectedVideo, setSelectedVideo] = useState<
+    VideosQueryResult[number] | null
+  >(null);
 
   const filteredData = data?.filter((video) => {
     if (!selectedProject) return true;
     return video.project?._id === selectedProject;
   });
+
+  const handleInfoClick = (video: VideosQueryResult[number]) => {
+    setIsInfoPanelOpen(true);
+    setSelectedVideo(video);
+  };
 
   if (isLoading || projectsDataLoading) return <Loading />;
   if (error) return <div>{error.message}</div>;
@@ -40,43 +42,45 @@ export default function Videos({ section }: { section: Section }) {
           {section.title[language] || section.title.es}
         </h1>
       )}
-      <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
+      <div className="grid grid-cols-1 gap-2 lg:grid-cols-3">
         {filteredData?.length ? (
           filteredData.map((video) =>
             video.embed ? (
-              <div key={video._id} className="border p-4">
-                {video.title?.es && (
-                  <h2>{video.title[language] || video.title.es}</h2>
-                )}
-                <p>{video.date ?? video.date}</p>
-                {video.category?.name?.es && (
-                  <div className="text-xs">
-                    {video.category.name[language] || video.category.name.es}
+              <div key={video._id} className="_max-h-80 p-4">
+                <div className="flex flex-col">
+                  {video.category?.name?.es && (
+                    <div className="text-xs">
+                      {video.category.name[language] || video.category.name.es}
+                    </div>
+                  )}
+                  <div className="flex flex-col items-start">
+                    {video.title?.es && (
+                      <h2 className="text-lg">
+                        {video.title[language] || video.title.es}
+                      </h2>
+                    )}
                   </div>
-                )}
-                <VideoPlayer embedData={video.embed} />
-                {video.text?.es && (
-                  <PortableText value={video.text[language] || video.text.es} />
-                )}
+                </div>
 
-                <NavLink
-                  to={`/${videosSection?.reference?.slug}/${video.slug?.current}`}
-                >
-                  {language === "es" ? "Ver más" : "See more"}
-                </NavLink>
+                <div className="h-55">
+                  <VideoPlayer embedData={video.embed} />
+                </div>
+                {video.text?.es && (
+                  <button onClick={() => handleInfoClick(video)}>INFO</button>
+                )}
               </div>
             ) : null,
           )
         ) : language === "es" ? (
-          <p>
-            No se hay {section.title?.es} de{" "}
+          <p className="lowercase">
+            No hay {section.title?.es} de{" "}
             {
               projectsData?.find((project) => project._id === selectedProject)
                 ?.title?.es
             }
           </p>
         ) : (
-          <p>
+          <p className="lowercase">
             There are no{" "}
             {
               projectsData?.find((project) => project._id === selectedProject)
@@ -84,6 +88,12 @@ export default function Videos({ section }: { section: Section }) {
             }{" "}
             {section?.title?.en || section.title?.es}
           </p>
+        )}
+        {isInfoPanelOpen && selectedVideo && (
+          <VideoInfoPanel
+            video={selectedVideo}
+            handleClose={() => setIsInfoPanelOpen(false)}
+          />
         )}
       </div>
     </>
