8287ae1283fd659a02c00f5d043fd6c6e1913663
astrosuka <yegresvoselok@gmail.com>
2026-01-14 19:28:24 -0300

    Create renderSectionLink function, add button style based on section type and include icons in NavMenuList

--- src/components/NavMenuList.tsx
+++ src/components/NavMenuList.tsx
@@ -4,7 +4,9 @@ import Loading from "../components/Loading";
 import { SECTION_IDS } from "../data/constants";
 import { useSectionSlug } from "../hooks/useSectionSlug";
 import useLanguage from "../hooks/useLanguage";
-import type { InitialDataQueryResult } from "../lib/types";
+import type { InitialDataQueryResult, Slug } from "../lib/types";
+import { Brush, Eye } from "lucide-react";
+import type { JSX } from "react";
 
 type InitialData = NonNullable<InitialDataQueryResult>;
 type Section = NonNullable<InitialData["sections"]>[number];
@@ -13,6 +15,84 @@ type NavMenuListProps = {
   data: Section[];
   className: string;
 };
+
+type PublicationsSlug = {
+  _id: string | null;
+  slug: Slug | null;
+};
+
+function renderSectionLink(
+  section: Section,
+  publicationsSlug: PublicationsSlug | undefined,
+  language: "es" | "en",
+): JSX.Element | null {
+  switch (section?.reference?._type) {
+    case "publication": {
+      if (!publicationsSlug?.slug?.current) return null;
+      if (!section.reference.slug) return null;
+
+      return (
+        <NavLink
+          to={`/${publicationsSlug.slug?.current}/${section.reference.slug}`}
+          className={({ isActive }) =>
+            `bg-accent rounded-xl border px-2 text-white transition-colors ${
+              isActive ? "" : ""
+            }`
+          }
+        >
+          {section.title?.es && (section.title[language] || section.title.es)}
+        </NavLink>
+      );
+    }
+
+    case "board": {
+      return (
+        <NavLink
+          to={`/${section.reference.slug}`}
+          className={({ isActive }) =>
+            `_size-18 flex cursor-pointer items-center justify-center rounded-xl border px-2 transition-colors hover:bg-yellow-400 hover:text-black ${
+              isActive ? "bg-yellow-400 text-black" : "bg-white"
+            }`
+          }
+        >
+          {section.title?.es && (section.title[language] || section.title.es)}
+          <Brush strokeWidth={0.75} size={42} />
+        </NavLink>
+      );
+    }
+
+    case "oraculo": {
+      return (
+        <NavLink
+          to={`/${section.reference.slug}`}
+          className={({ isActive }) =>
+            `flex cursor-pointer items-center justify-center rounded-xl border px-2 transition-colors hover:bg-pink-500 hover:text-white ${
+              isActive ? "bg-pink-500 text-white" : "bg-white"
+            }`
+          }
+        >
+          <Eye strokeWidth={0.75} size={48} />
+          {section.title?.es && (section.title[language] || section.title.es)}
+        </NavLink>
+      );
+    }
+
+    default: {
+      return (
+        <NavLink
+          to={`/${section?.reference?.slug}`}
+          className={({ isActive }) =>
+            `cursor-pointer rounded-xl border px-2 transition-colors hover:bg-black hover:text-white ${
+              isActive ? "bg-black text-white" : "bg-white"
+            }`
+          }
+        >
+          {section.title?.es && (section.title[language] || section.title.es)}
+        </NavLink>
+      );
+    }
+  }
+}
 
 export default function NavMenuList({ data, className }: NavMenuListProps) {
   const { data: sectionSlug, isLoading: sectionSlugLoading } = useSectionSlug();
@@ -25,61 +105,27 @@ export default function NavMenuList({ data, className }: NavMenuListProps) {
   );
 
   return (
-    <div className={className}>
-      {data.map((section, index) => {
-        if (section.isHidden) return;
+    <ul className={className}>
+      {data.map((section) => {
+        if (section.isHidden) return null;
 
-        const mid = data.length / 2;
         return (
-          <motion.li
-            drag
-            key={section.reference?._id || section.url}
-            style={{
-              transform: `translateY(${index < mid ? ((index - mid) / 2) * -8 : index + 1 - mid}px)`,
-            }}
-          >
-            {section.reference ? (
-              section.reference._type === "publication" && publicationsSlug ? (
-                <NavLink
-                  to={
-                    "/" +
-                    publicationsSlug.slug?.current +
-                    "/" +
-                    section.reference.slug
-                  }
-                  className={({ isActive }) =>
-                    `transition-colors ${isActive ? "underline" : ""} bg-yellow rounded-xl border bg-yellow-200 px-2`
-                  }
-                >
-                  {section.title?.es &&
-                    (section.title[language] || section.title?.es)}
-                </NavLink>
-              ) : (
-                <NavLink
-                  to={"/" + section.reference.slug}
-                  className={({ isActive }) =>
-                    `cursor-pointer rounded-xl border px-2 transition-colors hover:bg-black hover:text-white ${isActive ? "bg-black text-white" : "bg-white"}`
-                  }
-                >
-                  {section.title?.es &&
-                    (section.title[language] || section.title?.es)}
-                </NavLink>
-              )
-            ) : (
-              section.url && (
-                <a
-                  href={section.url}
-                  target="_blank"
-                  className="bg-blue-200/30"
-                >
-                  {section.title?.es &&
-                    (section.title[language] || section.title?.es)}
-                </a>
-              )
-            )}
+          <motion.li drag key={section.reference?._id || section.url}>
+            {section.reference
+              ? renderSectionLink(section, publicationsSlug, language)
+              : section.url && (
+                  <a
+                    href={section.url}
+                    target="_blank"
+                    className="bg-blue-200/30 underline"
+                  >
+                    {section.title?.es &&
+                      (section.title[language] || section.title?.es)}
+                  </a>
+                )}
           </motion.li>
         );
       })}
-    </div>
+    </ul>
   );
 }
