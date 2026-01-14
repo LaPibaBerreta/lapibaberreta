8bf68e2f09b83c34a5e047a56ebbf73c306bf458
astrosuka <yegresvoselok@gmail.com>
2026-01-14 19:18:47 -0300

    Improve home button position, change icon and add motion

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -1,18 +1,28 @@
-import { NavLink, useLocation } from "react-router";
+import { Link, useLocation } from "react-router";
+import { X } from "lucide-react";
+import { motion } from "motion/react";
 
 export default function HomeButton() {
   const location = useLocation();
 
   return (
-    <div className="fixed top-5 right-35 z-100">
+    <div className="fixed top-25 right-35 z-100">
       {location.pathname !== "/" && (
-        <NavLink
-          key="home"
-          to="/"
-          className={`hover:bg-accent pointer-events-auto flex size-18 cursor-pointer items-center justify-center gap-1 rounded-full border bg-white/40 text-4xl transition-colors hover:text-white`}
+        <motion.button
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
         >
-          <img src="icons/home.svg" className="size-12" />
-        </NavLink>
+          <Link
+            key="home"
+            to="/"
+            className={`hover:bg-accent _text-4xl _shadow-lg pointer-events-auto flex size-12 cursor-pointer items-center justify-center gap-1 rounded-full border bg-white/40 shadow-yellow-300/80 transition-colors hover:text-white`}
+          >
+            <X size={48} strokeWidth={1} />
+          </Link>
+        </motion.button>
       )}
     </div>
   );
