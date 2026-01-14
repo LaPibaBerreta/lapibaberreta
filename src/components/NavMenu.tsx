570e9fb0b95b39509280ba7c3a61db42c4463f06
astrosuka <yegresvoselok@gmail.com>
2026-01-14 19:26:59 -0300

    Improve style, add motion and icons in NavMenu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -2,7 +2,9 @@ import { useState, useEffect } from "react";
 import { useInitialData } from "../hooks/useInitialData";
 import NavMenuList from "./NavMenuList";
 import type { InitialDataQueryResult } from "../lib/types";
+import { SquareMenu, SquareX } from "lucide-react";
 import useIsMobile from "../hooks/useIsMobile";
+import { motion } from "motion/react";
 
 type InitialData = NonNullable<InitialDataQueryResult>;
 type Section = NonNullable<InitialData["sections"]>[number];
@@ -26,16 +28,23 @@ export default function NavMenu() {
 
   return (
     <nav className="_mb-5 _relative pointer-events-none fixed inset-0 z-100 font-mono">
-      <button
+      <motion.button
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
         onClick={() => {
           setIsOpen(!isOpen);
         }}
-        className={`${isOpen ? "bg-black" : ""} pointer-events-auto fixed right-5 bottom-5 z-100 flex size-12 items-center justify-center border border-black`}
+        className="pointer-events-auto fixed right-5 bottom-5 z-100 flex cursor-pointer items-center justify-center rounded-md"
       >
-        <div
-          className={`${isOpen ? "bg-white" : "bg-black"} size-10 rounded-full`}
-        />
-      </button>
+        {!isOpen ? (
+          <SquareMenu size={42} strokeWidth={0.75} fill="white" />
+        ) : (
+          <SquareX size={42} strokeWidth={0.75} fill="white" />
+        )}
+      </motion.button>
       {isOpen && (
         <ul className="mr-30 flex w-full flex-col items-center justify-center gap-3 text-lg sm:flex-row">
           {firstGroup?.length && (
@@ -55,7 +64,7 @@ export default function NavMenu() {
           {thirdGroup?.length && (
             <NavMenuList
               data={thirdGroup}
-              className="pointer-events-auto fixed top-1/2 right-5 text-end"
+              className="pointer-events-auto fixed top-1/2 right-5 flex flex-col gap-2 text-end"
             />
           )}
         </ul>
