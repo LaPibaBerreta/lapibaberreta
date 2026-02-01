971466c236b11931a2c344b9dc22151672a67149
astrosuka <yegresvoselok@gmail.com>
2026-02-01 01:09:46 -0300

    Add border and adjust size of ProjectIndicator

--- src/components/ProjectIndicator.tsx
+++ src/components/ProjectIndicator.tsx
@@ -1,3 +1,8 @@
 export default function ProjectIndicator({ color }: { color: string }) {
-  return <div style={{ background: "#" + color }} className="size-2" />;
+  return (
+    <div
+      style={{ background: "#" + color }}
+      className="size-3 rounded-full border"
+    />
+  );
 }
