ea5f194a08754669c12c66355a4780aa73458308
astrosuka <yegresvoselok@gmail.com>
2026-02-01 01:42:50 -0300

    Update accent color for consistency

--- src/components/BandcampPlayer.tsx
+++ src/components/BandcampPlayer.tsx
@@ -3,7 +3,7 @@ import { getBandcampAlbumId } from "../utils/getBandcampAlbumId";
 export default function BandcampPlayer({ embedData }: { embedData: string }) {
   const embedId = getBandcampAlbumId(embedData).albumId;
 
-  const embedUrl = `https://bandcamp.com/EmbeddedPlayer/album=${embedId}/size=large/bgcol=333333/linkcol=ffffff/artwork=false/transparent=true/tracklist=true`;
+  const embedUrl = `https://bandcamp.com/EmbeddedPlayer/album=${embedId}/size=large/bgcol=333333/linkcol=ff3300/artwork=false/transparent=true/tracklist=true`;
 
   return (
     <>
