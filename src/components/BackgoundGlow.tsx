f4bca902d74383c27ec6b9a8346bde8005e9410d
astrosuka <yegresvoselok@gmail.com>
2026-02-01 22:12:31 -0300

    Add motion and display gradient made of combined project colors to background

--- src/components/BackgoundGlow.tsx
+++ src/components/BackgoundGlow.tsx
@@ -1,5 +1,6 @@
 import { useProjects } from "../hooks/useProjects";
 import useFilterByProject from "../hooks/useFilterByProject";
+import { motion } from 'motion/react'
 
 export default function BackgroundGlow() {
   const { data, isLoading, error } = useProjects();
@@ -15,8 +16,16 @@ export default function BackgroundGlow() {
     ? data?.find((p: { _id: string }) => p._id === selectedProject)
     : undefined;
 
+  const homeBackground =
+    data
+      ?.filter((p) => p.color).reverse()
+      .map((p) => `#${p.color}`)
+      .join(", ")
+
   return (
-    <div
+    <motion.div
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
       className="pointer-events-none fixed inset-0 -z-10 h-screen w-full mask-t-from-50% mask-b-from-50% mask-radial-[20%_40%] mask-radial-from-30%"
       style={{
         background: selected
@@ -25,11 +34,11 @@ export default function BackgroundGlow() {
             #${selected?.color}cc 0%,
             transparent 60%
           )`
-          : // TODO: definir que hacer con el fondo aca, por el momento es transpaente
-            `radial-gradient(
+          :
+          `radial-gradient(
             circle at 50% 50%,
-            #ff1e1200 0%,
-            transparent 60%
+            ${homeBackground} 5%,
+            transparent 20%
           )`,
       }}
     />
