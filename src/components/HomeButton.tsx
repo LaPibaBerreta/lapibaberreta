e989406fa048d1f79c8b5acb7df54dbe8e21d0cc
astrosuka <yegresvoselok@gmail.com>
2026-02-01 22:15:08 -0300

    Enhance home button with different icon for mobile and variant for special cases

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -1,9 +1,11 @@
 import { Link, useLocation } from "react-router";
-import { X } from "lucide-react";
+import { X, HouseHeart } from "lucide-react";
 import { motion } from "motion/react";
+import useIsMobile from "../hooks/useIsMobile";
 
-export default function HomeButton({ className }: { className: string }) {
+export default function HomeButton({ className, variantX = false }: { className: string, variantX?: boolean }) {
   const location = useLocation();
+  const { isMobile } = useIsMobile()
 
   return (
     <div className={className}>
@@ -18,9 +20,15 @@ export default function HomeButton({ className }: { className: string }) {
           <Link
             key="home"
             to="/"
-            className="hover:bg-accent pointer-events-auto flex size-12 cursor-pointer items-center justify-center gap-1 rounded-full border bg-white transition-colors hover:text-white"
+            className="hover:bg-accent pointer-events-auto flex size-12 cursor-pointer items-center justify-center gap-1 rounded-full bg-black text-white transition-colors hover:text-white"
           >
-            <X size={48} strokeWidth={1} />
+            {variantX ?
+              <X size={48} strokeWidth={1} />
+              :
+              !isMobile ?
+                <X size={48} strokeWidth={1} />
+                : <HouseHeart size={36} strokeWidth={0.75} />
+            }
           </Link>
         </motion.button>
       )}
