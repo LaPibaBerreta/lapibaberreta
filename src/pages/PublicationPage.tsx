43ebc8ca3ccee970b0ec7603e83698ee13c1bafe
astrosuka <yegresvoselok@gmail.com>
2026-02-02 14:39:37 -0300

    Improve PublicationPage layout

--- src/pages/PublicationPage.tsx
+++ src/pages/PublicationPage.tsx
@@ -27,19 +27,28 @@ export default function PublicationPage({ section }: { section: Section }) {
   console.log("Section:", section);
 
   return (
-    <>
-      {data?.title?.es && (
-        <h1 className="text-xl">{data.title[language] || data.title.es}</h1>
-      )}
-      <p>{data?.date}</p>
-      {data?.category?.name?.es && (
-        <p>{data.category.name[language] || data.category.name.es}</p>
-      )}
+    <div>
+      <div className="flex items-center gap-2">
+        {data?.title?.es && (
+          <h1 className="font-secondary text-4xl">
+            {data.title[language] || data.title.es}
+          </h1>
+        )}
+        {data?.embed && <LoadToPlayerButton data={data.embed} />}
+      </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2">
+      <div className="flex gap-2">
+        <p>{data?.date}</p>
+        {data?.category?.name?.es && (
+          <p>{data.category.name[language] || data.category.name.es}</p>
+        )}
+      </div>
+
+      <div className="mb-8 grid grid-cols-1 gap-4 sm:grid-cols-2">
         <div>
           {data?.mainImage && (
             <img
+              className="rounded-2xl"
               src={
                 urlFor(data.mainImage).format("webp").width(600).url() +
                 "&fit=max"
@@ -48,53 +57,66 @@ export default function PublicationPage({ section }: { section: Section }) {
           )}
         </div>
         <div>
-          {data?.embed && <LoadToPlayerButton data={data.embed} />}
-
-          {data?.tracklist?.length &&
-            data.tracklist.map((track, index) => {
-              return <li key={index + track}>{track}</li>;
-            })}
-
-          {data?.text?.es && (
-            <PortableText value={data.text[language] || data.text.es} />
+          {data?.tracklist?.length ? (
+            <ul>
+              {data.tracklist.map((track, index) => {
+                return (
+                  <li key={index + track}>
+                    {index + 1}. {track}
+                  </li>
+                );
+              })}
+            </ul>
+          ) : (
+            data?.text?.es && (
+              <PortableText value={data.text[language] || data.text.es} />
+            )
           )}
-          {data?.links?.length &&
-            data.links.map((link) => (
-              <a
-                key={link._key}
-                href={link.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-              >
-                {link.title?.es && (link.title[language] || link.title.es)}
-              </a>
-            ))}
         </div>
       </div>
+      <div className="flex flex-col gap-8">
+        {data?.tracklist?.length && data?.text?.es && (
+          <PortableText value={data.text[language] || data.text.es} />
+        )}
 
-      {data?.imageGallery?.length && <ImageGallery data={data.imageGallery} />}
+        {data?.links?.length &&
+          data.links.map((link) => (
+            <a
+              key={link._key}
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline"
+            >
+              {link.title?.es && (link.title[language] || link.title.es)}
+            </a>
+          ))}
 
-      {data?.videos?.length &&
-        data.videos.map((video) => (
-          <div key={video._id} className="bg-violet-400">
-            {video?.title?.es && (
-              <h2>{video.title[language] || video.title.es}</h2>
-            )}
-            {video?.embed && <VideoPlayer embedData={video.embed} />}
+        {data?.imageGallery?.length && (
+          <ImageGallery data={data.imageGallery} />
+        )}
+
+        {data?.videos?.length && (
+          <div className="flex flex-col gap-4">
+            {data.videos.map((video) => (
+              <div key={video._id} className="h-100">
+                {video?.embed && <VideoPlayer embedData={video.embed} />}
+              </div>
+            ))}
           </div>
-        ))}
+        )}
 
-      {data?.additionalDocument && (
-        <div className="text-4xl underline">
-          <NavLink to={"/" + data.additionalDocument?.slug?.current}>
-            {data.additionalDocument.title?.es
-              ? data.additionalDocument.title[language] ||
-                data.additionalDocument.title.es
-              : "???"}
-          </NavLink>
-        </div>
-      )}
-    </>
+        {data?.additionalDocument && (
+          <div className="font-secondary my-8 text-center text-6xl underline">
+            <NavLink to={"/" + data.additionalDocument?.slug?.current}>
+              {data.additionalDocument.title?.es
+                ? data.additionalDocument.title[language] ||
+                  data.additionalDocument.title.es
+                : "???"}
+            </NavLink>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
